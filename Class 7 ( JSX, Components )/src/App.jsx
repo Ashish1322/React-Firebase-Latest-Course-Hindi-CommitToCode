d304d91b69287f4eb8856f4f43f80694563ac062
ashish4323 <ashish.k@fwc.co.in>
2023-10-13 19:23:49 +0530

    JSX & Components: Video 6,7

---
+++ Class 7 ( JSX, Components )/src/App.jsx
@@ -0,0 +1,11 @@
+import { Body } from "./components/Body.jsx";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header.jsx";
+import Temp from "./components/Temp.jsx";
+export function App() {
+  return (
+    <div>
+      <Temp />
+    </div>
+  );
+}
